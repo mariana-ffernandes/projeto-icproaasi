63a33ef1b4dfca5d621146fa449e10b7c1748316
mariferndes <marifernnds@gmail.com>
2024-06-15 13:06:14 -0300

    responsividade de telas

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,100 +1,98 @@
 <template>
-    <div class="home-page">
-      <div class="content-container">
-        <div class="left-side">
-          <div class="header-container">
-            <div class="logo-container">
-              <img src="@/assets/logoProAASI.png" alt="ProAASI Logo" class="logo" />
-            </div>
-            <h1 class="title">ProAASI</h1>
+  <div class="home-page">
+    <div class="content-container">
+      <div class="left-side">
+        <div class="header-container">
+          <div class="logo-container">
+            <img src="@/assets/logoProAASI.png" alt="Logotipo do ProAASI" class="logo" />
           </div>
-          <p class="subtitle">Programa de Orientação e Adaptação do Aparelho de Amplificação Sonora Individual</p>
+          <h1 class="title">Pro<br>AASI</h1>
         </div>
-        <div class="right-side">
-          <div class="buttons-wrapper">
-            <div class="buttons-container">
-              <router-link to="/login" class="button">Entrar</router-link>
-              <router-link to="/register" class="button">Cadastrar</router-link>
-              <router-link to="/terms" class="button">Sobre o ProAASI</router-link>
-            </div>
+        <p class="subtitle">Programa de Orientação e Adaptação do Aparelho de Amplificação Sonora Individual</p>
+      </div>
+      <div class="right-side">
+        <div class="buttons-wrapper">
+          <div class="buttons-container">
+            <router-link to="/login" class="button">Entrar</router-link>
+            <router-link to="/register" class="button">Cadastrar</router-link>
+            <router-link to="/terms" class="button">Sobre o ProAASI</router-link>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'HomePage'
-  }
-  </script>
-  
-  <style scoped>
-  .home-page {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background: url('@/assets/background.png') no-repeat center center;
-    background-size: cover;
-    padding: 20px 0px 20px 200px;
-  }
-  
-  .content-container {
-    display: contents;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    max-width: 1200px;
-  }
-  
-  .left-side {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 10px;
-  }
-  
-  .header-container {
-    display: flex;
-    align-items: center;
-    gap: 10px;
-  }
-  
-  .logo-container {
-    display: flex;
-    align-items: center;
-  }
-  
-  .logo {
-    width: 200px;
-  }
-  
-  .title {
-    color: var(--color-white);
-    font-family: 'Montserrat', sans-serif;
-    font-size: 6rem;
-    font-weight: 500;
-  }
-  
-  .subtitle {
-    color: var(--color-white);
-    font-family: 'Montserrat', sans-serif;
-    font-size: 3rem;
-    font-weight: 600;
-    text-align: center;
-    max-width: 80%;
-  }
-  
-  .right-side {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    gap: 10px;
-    flex: 1;
-  }
-  
-  .buttons-container {
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+.home-page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: url('@/assets/background.png') no-repeat center center;
+  background-size: cover;
+  padding: 20px;
+}
+
+.content-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1200px;
+  flex-wrap: wrap;
+}
+
+.left-side {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  flex: 1;
+}
+
+.header-container {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.logo {
+  width: 200px;
+}
+
+.title {
+  color: var(--color-white);
+  font-family: 'Montserrat', sans-serif;
+  font-size: 6rem;
+  font-weight: 500;
+  line-height: 1; 
+}
+
+.subtitle {
+  color: var(--color-white);
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.7rem;
+  font-weight: 600;
+  text-align: center;
+  max-width: 80%;
+}
+
+.right-side {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 10px;
+  flex: 1;
+}
+
+.buttons-container {
   width: 35rem;
   margin-top: 40px;
   display: flex;
@@ -104,12 +102,12 @@
   padding: 30px;
   border-radius: none;
 }
-  
-  .button {
+
+.button {
   font-family: 'Montserrat', sans-serif;
   font-size: 1.5rem;
   font-weight: 500;
-  color: var(--color-black)000;
+  color: var(--color-black);
   margin: 10px 0;
   width: 80%;
   max-width: 30rem;
@@ -119,11 +117,111 @@
   box-shadow: 5px 6px 1px 1px var(--color-shadow-button);
   cursor: pointer;
   text-align: center;
+}
+
+.button:hover {
+  background-color: var(--color-background);
+  color: #333;
+}
+
+@media (max-width: 1024px) {
+  .content-container {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+
+  .left-side {
+    align-items: center;
+  }
+
+  .title {
+    font-size: 5rem;
   }
   
-  .button:hover {
-    background-color: var(--color-background);
-    color: #333;
+  .subtitle {
+    color: var(--color-white);
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.9rem;
+    font-weight: 600;
+    text-align: center;
+    max-width: 80%;
   }
-  </style>
-  
+
+  .buttons-container {
+    width: 30rem;
+  }
+
+  .button {
+    font-size: 2rem;
+  }
+}
+
+@media (max-width: 768px) {
+  .content-container {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+
+  .left-side {
+    align-items: center;
+  }
+
+  .logo {
+    width: 10rem;
+  }
+
+  .title {
+    font-size: 5rem;
+  }
+
+  .subtitle {
+    color: var(--color-white);
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.9rem;
+    font-weight: 600;
+    text-align: center;
+    max-width: 80%;
+  }
+
+  .buttons-container {
+    width: 25rem;
+  }
+
+  .button {
+    font-size: 1.5rem;
+    width: 90%;
+  }
+}
+
+@media (max-width: 480px) {
+  .logo {
+    width: 150px; 
+  }
+
+  .title {
+    font-size: 4rem;
+  }
+
+  .subtitle {
+    color: var(--color-white);
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.2rem;
+    font-weight: 600;
+    text-align: center;
+    max-width: 80%;
+  }
+
+  .buttons-container {
+    width: 100%;
+    padding: 20px;
+  }
+
+  .button {
+    font-size: 2rem;
+    padding: 10px;
+    width: 100%;
+  }
+}
+</style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -50,19 +50,19 @@ export default {
   }
 }
 </script>
-  
-  <style scoped>
-  .login-page {
-    display: flex;
-    flex-direction: column;
-    align-items: center;    
-    height: 100vh;
-    background-color: var(--color-background);
-    padding: 20px;
-    margin-top: 100px;
-  }
 
-  .header {
+<style scoped>
+.login-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  background-color: var(--color-background);
+  padding: 20px;
+  margin-top: 100px;
+}
+
+.header {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -81,106 +81,183 @@ export default {
 
 .header-title {
   color: var(--color-black);
-  font-family: 'Montserrat', sans-serif; 
+  font-family: 'Montserrat', sans-serif;
   font-size: 3rem;
   font-weight: 500;
   margin: 0;
 }
-  
-  .login-container {
-    background-color: var(--color-login-container);
-    padding: 40px;
-    border-radius: 0px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 40rem;
-    height: 29rem;
-  }
-  
-  .title {
-    color: var(--color-txt-title);
-    font-size: 1.5rem;
-    font-weight: bold;
-    margin-bottom: 20px;
-  }
-  
-  .form-group {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 15px;
-    width: 100%;
-  }
-  
-  label {
-    margin-bottom: 5px;
-    font-weight: bold;
-  }
-  
-  .form-control {
-    padding: 10px;
-    background-color: transparent;
-    border: 1px solid var(--color-black);
-    border-radius: 0px;
-    font-size: 1rem;
-    width: 100%;
-  }
 
-  .form-control::placeholder {
+.login-container {
+  background-color: var(--color-login-container);
+  padding: 40px;
+  border-radius: 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40rem;
+  height: 29rem;
+}
+
+.title {
+  color: var(--color-txt-title);
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+  width: 100%;
+}
+
+label {
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+.form-control {
+  padding: 10px;
+  background-color: transparent;
+  border: 1px solid var(--color-black);
+  border-radius: 0px;
+  font-size: 1rem;
+  width: 100%;
+}
+
+.form-control::placeholder {
   text-align: center;
   color: var(--color-black);
 }
-  
-  .login-button {
-    background-color: var(--color-background);
-    color: var(--color-black);
-    padding: 10px 20px;
-    text-decoration: none;
-    font-family: 'Montserrat', sans-serif;
-    font-size: 1.2rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 0px;
-    box-shadow: 2px 3px 1px 1px var(--color-shadow-button);
-    margin-top: 20px;
-    margin-bottom: 80px;
-    cursor: pointer;
-    transition: background-color 0.3s, color 0.3s;
-    width: 20rem;
+
+.login-button {
+  background-color: var(--color-background);
+  color: var(--color-black);
+  padding: 10px 20px;
+  text-decoration: none;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.2rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 0px;
+  box-shadow: 2px 3px 1px 1px var(--color-shadow-button);
+  margin-top: 20px;
+  margin-bottom: 80px;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s;
+  width: 20rem;
+}
+
+.login-button:hover {
+  background-color: var(--color-background);
+  color: #333;
+}
+
+.extra-buttons {
+  display: flex;
+  gap: 30px;
+}
+
+.extra-button {
+  background-color: var(--color-background);
+  color: var(--color-black);
+  padding: 10px 20px;
+  text-decoration: none;
+  text-align: center;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.2rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 0px;
+  box-shadow: 2px 3px 1px 1px var(--color-shadow-button);
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s;
+  width: 15rem;
+}
+
+.extra-button:hover {
+  background-color: var(--color-background);
+  color: #333;
+}
+
+@media (max-width: 1024px) {
+  .header-title {
+    font-size: 2.5rem;
   }
   
-  .login-button:hover {
-    background-color: var(--color-background);
-    color: #333;
-  }
-  
-  .extra-buttons {
-    display: flex;
-    gap: 30px;
-  }
-  
-  .extra-button {
-    background-color: var(--color-background);
-    color: var(--color-black);
-    padding: 10px 20px;
-    text-decoration: none;
-    text-align: center;
-    font-family: 'Montserrat', sans-serif;
-    font-size: 1.2rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 0px;
-    box-shadow: 2px 3px 1px 1px var(--color-shadow-button);
-    cursor: pointer;
-    transition: background-color 0.3s, color 0.3s;
-    width: 15rem;
-  }
-  
-  .extra-button:hover {
-    background-color: var(--color-background);
-    color: #333;
+  .login-container {
+    width: 80%;
+    height: auto;
   }
 
-  
-  </style>
-  
+  .login-button {
+    width: 80%;
+  }
+
+  .extra-button {
+    width: 80%;
+  }
+}
+
+@media (max-width: 768px) {
+  .header-title {
+    font-size: 2rem;
+  }
+
+  .logo {
+    width: 60px;
+  }
+
+  .login-container {
+    padding: 20px;
+    width: 100%;
+  }
+
+  .login-button {
+    width: 100%;
+  }
+
+  .extra-buttons {
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .extra-button {
+    width: 90%;
+  }
+}
+
+@media (max-width: 480px) {
+  .header-title {
+    font-size: 3rem;
+  }
+
+  .logo {
+    width: 80px;
+  }
+
+  .login-container {
+    padding: 10px;
+    width: 95%;
+    height: 58%;
+  }
+
+  .form-control {
+    font-size: 1.3rem;
+    width: 100%;
+  }
+
+  .login-button {
+    width: 100%;
+    max-width: 17rem;
+    font-size: 1.5rem;
+  }
+
+  .extra-button {
+    width: 100%;
+    max-width: 19rem;
+    font-size: 1.5rem;
+  }
+}
+</style>
