afeffccb103c5172041b7ae3aa0c73bc795ba0c0
mariferndes <marifernnds@gmail.com>
2024-08-05 20:08:46 -0300

    ajustes finais de videos e textos.

--- src/videos.js
+++ src/videos.js
@@ -1,12 +1,12 @@
 export const videos = {
   "intra-aural": {
-    desligarLigar: '',
-    retirarColocarPilhas: '',
-    tamanhoComprarPilha: '',
+    desligarLigar: 'https://www.youtube.com/embed/OYOD6-oLv_8',
+    retirarColocarPilhas: 'https://www.youtube.com/embed/tJP5eVK0KsU',
+    tamanhoComprarPilha: 'https://www.youtube.com/embed/xuH168uuZ1w',
     pilhaAcabando: '',
     colocarRetirarAasi: 'https://www.youtube.com/embed/DVrBdp6_2fE',
-    limparAASIs: '',
-    trocarFiltrosCera: '',
+    limparAASIs: 'https://www.youtube.com/embed/jHFrgJznaNY',
+    trocarFiltrosCera: 'https://www.youtube.com/embed/6do8LyS3n0k',
     identificarOrelha: 'https://www.youtube.com/embed/HdOPVuvl8QU',
     guardarAparelho: 'https://www.youtube.com/embed/J4VwoQAq6qI'
   },
@@ -20,8 +20,8 @@ export const videos = {
     limparMoldes: 'https://www.youtube.com/embed/56U8hqFaJtM',
     limparOliva: 'https://www.youtube.com/embed/BhJT1FwyuMM',
     trocarTubos: '',
-    identificarOrelha: '',
-    guardarAparelho: ''
+    identificarOrelha: 'https://www.youtube.com/embed/9d3Jl6dqa1A',
+    guardarAparelho: 'https://www.youtube.com/embed/v5VcuauXmrQ'
   },
   "retroauricular-receptor": {
     desligarLigar: 'https://www.youtube.com/embed/X2vK_XAUuxU',
